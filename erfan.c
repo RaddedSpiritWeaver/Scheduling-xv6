2a00ce7a278069f16cde8074ceedcdaf4e256a8b
Erfan Noohi <erfan.noohi@gmail.com>
2019-04-13 00:23:26 +0430

    seems erfan.c was not added ? so lets do it again

--- erfan.c
+++ erfan.c
@@ -6,6 +6,6 @@ char buf[512];
 
 int main(int argc, char *argv[])
 {
-  printf(1,"hello world its erfan\nexit with ctrl a then x after release");
+  printf(1,"hello world its erfan\nexit with ctrl a then x after release\n");
   exit();
 }
