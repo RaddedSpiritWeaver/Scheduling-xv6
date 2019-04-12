2a46b39f935927016a536c1e1aa6ed31e9ad46b5
Erfan Noohi <erfan.noohi@gmail.com>
2019-04-13 00:21:15 +0430

    first official commit

---
+++ erfan.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+char buf[512];
+
+int main(int argc, char *argv[])
+{
+  printf(1,"hello world its erfan\nexit with ctrl a then x after release");
+  exit();
+}
