a9a72bb69987d78e7698f6f76e84db44a90e7c83
Erfan Noohi <erfan.noohi@gmail.com>
2019-04-13 11:08:51 +0430

    used the first program in the book to create a child

--- erfan.c
+++ erfan.c
@@ -7,5 +7,22 @@ char buf[512];
 int main(int argc, char *argv[])
 {
   printf(1,"hello world its erfan\nexit with ctrl a then x after release\n");
+  // now lets add the first program mentioned in the book
+  int pid = fork();
+  if(pid > 0){
+    printf(1,"parent: child=%d\n", pid);
+    pid = wait();
+    printf(1,"child with number %d is done\n");
+  }
+  else if (pid == 0)
+  {
+    printf(1,"hello this is a child\n");
+    exit();
+  }
+  else
+  {
+    printf(1,"error in fork\n");
+    exit();
+  }
   exit();
 }
